6aedba0cae3eb2ab586ea6c3bddd675cce9cbc09
DaehanHong <45205334+DaehanHong@users.noreply.github.com>
2019-10-14 18:49:24 -0400

    Add files via upload

---
+++ WordGramClassStarterProgram/MarkovZero.java
@@ -0,0 +1,46 @@
+/**
+ * Interface and Abstract Class
+ * Assignment 1: IMarkovModel Interface
+ * Assignment 2: AbstractMarkovModel Abstract Class
+ * 
+ * @author Kuei
+ * @version July 17, 2016
+ */
+
+import java.util.Random;
+
+//public class MarkovZero implements IMarkovModel {
+public class MarkovZero extends AbstractMarkovModel {
+//    private String myText;
+//	private Random myRandom;
+	
+//	public MarkovZero() {
+//		myRandom = new Random();
+//	}
+//	
+//	public void setRandom(int seed){
+//		myRandom = new Random(seed);
+//	}
+//	
+//	public void setTraining(String s){
+//		myText = s.trim();
+//	}
+	
+	public String getRandomText(int numChars){
+		if (myText == null){
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int k=0; k < numChars; k++){
+			int index = myRandom.nextInt(myText.length());
+			sb.append(myText.charAt(index));
+		}
+		
+		return sb.toString();
+	}
+	
+	@Override
+	public String toString(){
+		return String.format("MarkovModel of order %d",0);
+	}
+}
---
+++ WordGramClassStarterProgram/TesterTest.java
@@ -0,0 +1,38 @@
+
+/**
+ * Generating Random Text
+ * Assignment 1: MarkovZero and MarkovOne
+ * Assignment 2: MarkovFour and MarkovModel
+ * 
+ * @author Kuei
+ * @version July 11, 2016
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesterTest {
+	private Tester t;
+
+	@Before
+	public void setUp() throws Exception {
+		t = new Tester();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		t = null;
+	}
+
+	@Test
+	public void GetFollowstest() {
+		t.testGetFollows();
+	}
+	
+	@Test
+	public void GetFollowsWithFileTest() {
+		t.testGetFollowsWithFile();
+	}
+
+}
