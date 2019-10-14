ae75132fa9d6a07f2574d8ff9863f8d8aa3ea05c
DaehanHong <45205334+DaehanHong@users.noreply.github.com>
2019-10-14 18:40:25 -0400

    Add files via upload

---
+++ EarthquakeSortStarterProgram/QuakeSortInPlaceTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuakeSortInPlaceTest {
+	private QuakeSortInPlace qs;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.qs = new QuakeSortInPlace();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.qs = null;
+	}
+
+	@Test
+	public void test() {
+		qs.testSort();
+	}
+
+}
